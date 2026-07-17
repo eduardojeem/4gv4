a1d187245ef28b1bc6d3015b69c05dcc478605a4
Eduardoespinoza <johneduardoespinoza95@gmail.com>
2026-07-16 22:54:56 -0300

    fix: actualizar page principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export default function RootPage() {
-  // Redirigir siempre al portal público
-  redirect('/saas')
+  // Redirigir siempre al marketplace
+  redirect('/marketplace')
 }
